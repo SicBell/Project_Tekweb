d65aecc90c99b2c3b02e7058071ce77b272a0fb5
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2023-11-19 17:12:22 +0700

    Carousel index (kurang text welcome)

--- Index.php
+++ Index.php
@@ -7,11 +7,30 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
-   <?php require "header.php"; ?>
-
+    <?php require "header.php"; ?>
+    <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" width="100%">
+                <img src="asset/background_login.jpg" class="img-fluid d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="asset/background_login.jpg" class="img-fluid d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="asset/background_login.jpg" class="img-fluid d-block" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
+        </button>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
