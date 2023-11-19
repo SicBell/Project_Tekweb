da30e912a9af11dbc873f25736a6fdaab39761e7
SicBell <131933766+SicBell@users.noreply.github.com>
2023-11-19 15:51:17 +0700

    Commit 3

--- db_connect.php
+++ db_connect.php
@@ -2,12 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "accounts";
-try {
-	$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-	echo "Connection failed: " . $e->getMessage();
-	die;
+$dbname = "library";
+$conn = new mysqli($servername, $username, $password,$dbname,3306);
+if ($conn->connect_error) {
+	die("Connection Failed".$conn->connect_error);	
 }
+echo""
 ?>
