5cf5776c11440365ffb356567dc3c6f5e9b90a1d
SicBell <131933766+SicBell@users.noreply.github.com>
2023-11-24 16:40:56 +0700

    commit 10

--- Forgot_password_page.php
+++ Forgot_password_page.php
@@ -1,22 +1,60 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+
 <head>
-    <title>Forgot Password</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <style>
+        body {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #EA906C;
+        }
+
+        .card {
+            width: 500px;
+            /* Set your desired width */
+            height: 600px;
+            /* Make it square */
+        }
+
+        #btn-send{
+            background-color: #F8F0E5;
+        }
+
+        #btn-send:hover {
+            background-color: #EADBC8;
+        }
+    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
 
-    <h1>Forgot Password</h1>
+<body style=" background-color: #FFC5C5;">
 
-    <form method="post" action="send-password-reset.php">
 
-        <label for="email">email</label>
-        <input type="email" name="email" id="email">
+    <div class="card">
+        <div class="card-header" style="background-color: #E0F4FF">
+            Forgot Password
+        </div>
+        <div class="card-body" style="background-color: #F4BF96;">
+            <form method="post" action="send-password-reset.php">
 
-        <button>Send</button>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+                </div>
 
-    </form>
+                <input id="btn-send" class="btn" type="submit" value="Send">
+
+            </form>
+        </div>
+    </div>
+
 
 </body>
+
 </html>
--- process-reset-password.php
+++ process-reset-password.php
@@ -1,59 +1,90 @@
 <?php
 
-$token = $_POST["token"];
+// $token = $_POST["token"];
 
-$token_hash = hash("sha256", $token);
+// $token_hash = hash("sha256", $token);
 
-$mysqli = require __DIR__ . "/db_connect.php";
+// $mysqli = require __DIR__ . "/db_connect.php";
 
-$sql = "SELECT * FROM accounts
-        WHERE reset_token_hash = ?";
+// $sql = "SELECT * FROM accounts
+//         WHERE reset_token_hash = ?";
 
-$stmt = $mysqli->prepare($sql);
+// $stmt = $mysqli->prepare($sql);
 
-$stmt->bind_param("s", $token_hash);
+// $stmt->bind_param("s", $token_hash);
 
-$stmt->execute();
+// $stmt->execute();
 
-$result = $stmt->get_result();
+// $result = $stmt->get_result();
 
-$user = $result->fetch_assoc();
+// $user = $result->fetch_assoc();
 
-if ($user === null) {
-    die("token not found");
-}
+// if ($user === null) {
+//     die("token not found");
+// }
 
-if (strtotime($user["reset_token_expires_at"]) <= time()) {
-    die("token has expired");
-}
+// if (strtotime($user["reset_token_expires_at"]) <= time()) {
+//     die("token has expired");
+// }
 
-if (strlen($_POST["password"]) < 8) {
-    die("Password must be at least 8 characters");
-}
+// if ($_POST["password"] !== $_POST["password_confirmation"]) {
+//     die("Passwords must match");
+// }
 
-if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
-    die("Password must contain at least one letter");
-}
+// $password = $_POST['password'];
+// $sql = "UPDATE accounts
+//         SET password = ?,
+//             reset_token_hash = NULL,
+//             reset_token_expires_at = NULL
+//         WHERE Id = ?";
 
-if ( ! preg_match("/[0-9]/", $_POST["password"])) {
-    die("Password must contain at least one number");
-}
+// $stmt = $mysqli->prepare($sql);
 
-if ($_POST["password"] !== $_POST["password_confirmation"]) {
-    die("Passwords must match");
-}
+// $stmt->bind_param("ss", $password, $user["Id"]);
 
-$password = $_POST['password'];
-$sql = "UPDATE accounts
-        SET password = ?,
-            reset_token_hash = NULL,
-            reset_token_expires_at = NULL
-        WHERE Id = ?";
+// $stmt->execute();
 
-$stmt = $mysqli->prepare($sql);
 
-$stmt->bind_param("ss", $password, $user["Id"]);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+  <style>
+    body {
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #EA906C;
+    }
 
-$stmt->execute();
-
-echo "Password updated. You can now login.";
+    .card {
+      width: 500px; /* Set your desired width */
+      height: 600px; /* Make it square */
+    }
+    #btn-login{
+        background-color: #F8F0E5;
+    }
+    #btn-login:hover{
+        background-color: #EADBC8;
+    }
+  </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body style=" background-color: #FFC5C5;">
+  <div class="card">
+  <div  class="card-header" style="background-color: #E0F4FF" >
+    Notification!
+  </div>
+  <div class="card-body" style="background-color: #F4BF96;" >
+    <h5 class="card-title">Password updated. You can now login.</h5>
+    
+    <a href="login_page.php"  id="btn-login" class="btn ">Login</a>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
