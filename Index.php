c7ea068a61825718b9704a6fa231354855fdf963
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2023-12-14 14:55:15 +0700

    sql update + minor change

--- Index.php
+++ Index.php
@@ -117,12 +117,6 @@ $mysqli->close();
             <div class="carousel-item welcome-image active" width="100%">
                 <img src="asset/background_login.jpg" class="img-fluid d-block" alt="...">
             </div>
-            <div class="carousel-item welcome-image">
-                <img src="asset/background_login.jpg" class="img-fluid d-block" alt="...">
-            </div>
-            <div class="carousel-item welcome-image">
-                <img src="asset/background_login.jpg" class="img-fluid d-block" alt="...">
-            </div>
             <div class="carousel-text text-white d-flex justify-content-center">
                 <h1>WELCOME,
                     <?php echo ucfirst($username); ?>
