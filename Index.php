09a4dca63892368c1f97192e07147b2f2f88dc9d
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2023-12-15 21:10:39 +0700

    problem fix book status

--- Index.php
+++ Index.php
@@ -180,60 +180,10 @@ $mysqli->close();
             }
             ?>
         </div>
-        <div class="row book-result hstack d-flex justify-content-center">
+        <div class="row book-result mb-3 hstack d-flex justify-content-center">
             <!-- <div class="col-lg-12 col-md-3 col-sm-6"> -->
             <div class="col-12 hstack">
-                <?php foreach ($books as $book): ?>
-                    <?php if ($book['book_status'] !== 'borrowed'): ?>
-                        <!-- <div class="col-lg-4 col-md-3 col-sm-6"> -->
-                        <div class="col-3 me-2">
-                            <div class="card">
-                                <img data-bs-target="#book<?php echo $book['id']; ?>" data-bs-toggle="modal"
-                                    src="img/<?php echo $book['gambar']; ?>" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <div class="modal fade" id="book<?php echo $book['id']; ?>" tabindex="-1"
-                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <h3 style="text-align: center; font-size: 20px; color: darkblue;"
-                                                        class="uts">
-                                                        <?php echo $book['title']; ?>
-                                                    </h3>
-                                                    <p style="text-align: center; font-size: 20px; color: darkblue;"
-                                                        class="uts">
-                                                        ----★----</p>
-                                                    <img src="img/<?php echo $book['gambar']; ?>" class="card-img-top"
-                                                        alt="...">
-                                                    <h4 style="text-align:center">SYNOPSIS</h4>
-                                                    <p style="text-align: center; font-size: 20px; color: darkblue;"
-                                                        class="uts">
-                                                        <?php echo $book['sinopsis']; ?>
-                                                    </p>
-                                                </div>
-
-                                                <div class="modal-footer">
-                                                    <button type="button" style="color: blue; align-items: center;"
-                                                        class="btn btn-primary" data-bs-dismiss="modal">Close Window</button>
-                                                      
-                                                    <button type="button" class="btn btn-primary"
-                                                        onclick="redirectToBorrowForm(<?php echo $book['id']; ?>)">
-                                                        Borrow Book
-                                                    </button>
-                                                </a>    
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                <?php endforeach; ?>
+                
             </div>
         </div>
     </div>
@@ -355,7 +305,7 @@ $mysqli->close();
 
         $(".All").click(function () {
             // if (F == "false") {
-            inputButton = "ready"
+            inputButton = "available"
             $.ajax({
                 url: "showBooks.php",
                 method: "POST",
@@ -524,7 +474,7 @@ $mysqli->close();
 
 
         $(document).ready(function () {
-            var input = "ready";
+            var input = "available";
             <?php if (isset($_SESSION['username'])) { ?>
                 $.ajax({
                     url: "showBooks.php",
@@ -551,7 +501,7 @@ $mysqli->close();
                         }
                     });
                 } else {
-                    input = "ready";
+                    input = "available";
                     $.ajax({
                         url: "showBooks.php",
                         method: "POST",
